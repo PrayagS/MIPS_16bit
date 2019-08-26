766a4882c44cec8cadcedfe167feb54811941a0c
PrayagS <prayag.s@ahduni.edu.in>
2019-08-26 15:22:07 +0530

    Cleaned the ALU testbench

--- execution_block_tb.v
+++ execution_block_tb.v
@@ -72,224 +72,91 @@ module execution_block_tb;
 		op_dec = 0;
 		data_in = 16'h0008;
 		
-		// #5 clk = ~clk;
-		
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 1;
-		data_in = 16'h0008;
-
-		// #5 clk = ~clk;
 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 2;
-		data_in =16'h0008;
-		
-		// #5 clk = ~clk;
 		
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 4;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 5;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 6;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 7;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 8;
-		data_in = 16'h0008;
 		
-		// #5 clk = ~clk;
-		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 9;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 10;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 12;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 13;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 14;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
-		 
+
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 15;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 16;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 17;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 20;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 21;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 22;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 23;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'h4000;
-		B = 16'hC000;
 		op_dec = 24;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
 		A = 16'hC000;
 		B = 16'h0001;
 		op_dec = 25;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 26;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 27;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 28;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 29;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 30;
-		data_in = 16'h0008;
-		
-		// #5 clk = ~clk;
 		 
 		#10;
-		A = 16'hC000;
-		B = 16'h0001;
 		op_dec = 31;
-		data_in = 16'h0008;
 		 
 		//Added for self-testing
-		#10;
+		#10;		
 		A = 16'h4000;
 		B = 16'hC000;
 		op_dec = 1;
